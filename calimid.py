46f8f0a15e8ec8570953f1ca96b2a7f020fedf36
jmunkeby19 <34284770+jmunkeby19@users.noreply.github.com>
2019-03-04 08:28:37 -0800

    Update calimid.py

--- calimid.py
+++ calimid.py
@@ -1,9 +1,10 @@
 import Voss_RoboPiLib as RPL
 RPL.RoboPiInit(device = "/dev/ttyAMA0", bps = 115200)
 
+speed = 2000
 
 def calibrate():
   RPL.pinMode(0, RPL.PWM)
-  RPL.pwmWrite(0, 2000 * 2, 3000)
+  RPL.pwmWrite(0, speed, speed * 2)
 
 calibrate()
