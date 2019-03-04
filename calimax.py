89d7e49f5e626b2f877840489094b71bda23a24b
jmunkeby19 <34284770+jmunkeby19@users.noreply.github.com>
2019-03-04 08:27:32 -0800

    Update calimax.py

--- calimax.py
+++ calimax.py
@@ -1,9 +1,10 @@
 import Voss_RoboPiLib as RPL
 RPL.RoboPiInit(device = "/dev/ttyAMA0", bps = 115200)
 
+speed = 3000
 
 def calibrate():
   RPL.pinMode(0, RPL.PWM)
-  RPL.pwmWrite(0, 3000 * 2, 3000)
+  RPL.pwmWrite(0, speed * 2, speed)
 
 calibrate()
