95fd555f17c2eaf9b3746f55adb4a06c6b8ec8e7
jmunkeby19 <34284770+jmunkeby19@users.noreply.github.com>
2019-03-04 08:30:48 -0800

    Update calimin.py

--- calimin.py
+++ calimin.py
@@ -1,9 +1,10 @@
 import Voss_RoboPiLib as RPL
 RPL.RoboPiInit(device = "/dev/ttyAMA0", bps = 115200)
 
+speed = 1000
 
 def calibrate():
   RPL.pinMode(0, RPL.PWM)
-  RPL.pwmWrite(0, 1000 * 2, 3000)
+  RPL.pwmWrite(0, speed * 2, speed)   #RPL.pwmWrite(0, 1000 * 2, 3000)
 
 calibrate()
