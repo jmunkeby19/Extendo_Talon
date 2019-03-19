e0b3036cd3c316c7dfc8f7b01ea8d826c0c65520
jmunkeby19 <34284770+jmunkeby19@users.noreply.github.com>
2019-03-19 15:13:40 -0500

    Update keybind.py

--- keybind.py
+++ keybind.py
@@ -7,11 +7,28 @@ screen = curses.initscr()
 curses.halfdelay(5)
 curses.noecho()
 
+#2.2 inches/second times
 t1 = 4.5454
 t2 = 2.2727
 t3 = 0.4545
 t4 = 0.1136
 t5 = 0.3425
+
+#2.1 inches/second times
+#t1 = 4.762
+#t2 = 2.381
+#t3 = 0.476
+#t4 = 0.060
+#t5 = 0.359
+
+#2.3 inches/second times
+#t1 = 4.348
+#t2 = 2.174
+#t3 = 0.435
+#t4 = 0.109
+#t5 = 0.328
+
+#Original times for 2.0584 inches/second
 #t1 = 4.62325
 #t2 = 0.462325
 #t3 = 0.231625
