90b2c4a7f088fb313bf9862c6422f09d2afa5628
jmunkeby19 <34284770+jmunkeby19@users.noreply.github.com>
2019-03-21 12:38:29 -0500

    Update Extendo_math.py

--- Extendo_math.py
+++ Extendo_math.py
@@ -2,13 +2,13 @@ import Voss_RoboPiLib as RPL
 from time import sleep
 RPL.RoboPiInit(device = "/dev/ttyAMA0", bps = 115200)
 rate = 2
-speed = 500
 def movement():
   tm = abs(rate / distance)
   if distance > 0:
-    RPL.pinMode(0, RPL.PWM)
+    speed = 500
   else:
-    RPL.pinMode(1, RPL.PWM)
+    speed = 2000
+  RPL.pinMode(0, RPL.PWM)
   RPL.pwmWrite(0, speed, speed * 2)
   sleep(tm)
   RPL.pwmWrite(0, 0, 1)
