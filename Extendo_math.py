c872cd6bfb468f8353d9ed72601eed1e31bed989
jmunkeby19 <34284770+jmunkeby19@users.noreply.github.com>
2019-03-21 12:42:04 -0500

    Update Extendo_math.py

--- Extendo_math.py
+++ Extendo_math.py
@@ -3,7 +3,7 @@ from time import sleep
 RPL.RoboPiInit(device = "/dev/ttyAMA0", bps = 115200)
 rate = 2
 def movement():
-  tm = abs(rate / distance)
+  tm = distance / rate
   if distance > 0:
     speed = 500
   else:
