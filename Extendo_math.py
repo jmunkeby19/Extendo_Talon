cf6df52816e42935b6e88f5a76d0f3b8f38a74cd
jmunkeby19 <34284770+jmunkeby19@users.noreply.github.com>
2019-03-21 12:52:21 -0500

    Update Extendo_math.py

--- Extendo_math.py
+++ Extendo_math.py
@@ -1,14 +1,15 @@
 import Voss_RoboPiLib as RPL
+import fractions
 from time import sleep
 RPL.RoboPiInit(device = "/dev/ttyAMA0", bps = 115200)
 rate = 2
 def movement():
-  tm = round(distance / rate, 5)
+  tm = fractions.Fraction(distance, rate)
   if distance > 0:
     speed = 500
   else:
     speed = 2000
-  print(round(tm, 5))
+  print(tm)
   RPL.pinMode(0, RPL.PWM)
   RPL.pwmWrite(0, speed, speed * 2)
   sleep(abs(tm))
