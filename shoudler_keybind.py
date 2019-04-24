6b042d94a52dec4a8d10b60d084a49965c85264f
jmunkeby19 <34284770+jmunkeby19@users.noreply.github.com>
2019-04-24 11:30:53 -0500

    Create shoudler_keybind.py

---
+++ shoudler_keybind.py
@@ -0,0 +1,23 @@
+import _____ as RPL
+import time
+import curses
+RPL._______
+
+screen = curses.initscr()
+curses.halfdelay(5)
+curses.noecho()
+
+speed1 = 
+speed2 = 
+
+def clockwise():
+  RPL.
+  RPL.
+
+def counterclockwise():
+  RPL.
+  RPL.
+
+clockwise()
+
+counterclockwise()
