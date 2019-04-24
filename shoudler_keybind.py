82a1af9affed8f7b68928134d68db70bc80fea53
jmunkeby19 <34284770+jmunkeby19@users.noreply.github.com>
2019-04-24 11:38:37 -0500

    Update shoudler_keybind.py

--- shoudler_keybind.py
+++ shoudler_keybind.py
@@ -7,17 +7,28 @@ screen = curses.initscr()
 curses.halfdelay(5)
 curses.noecho()
 
-speed1 = 
-speed2 = 
+speed1 =      #Motor forwards
+speed2 =      #Motor backwards
+t1 =          #time.sleep
+t2 =          #time.sleep
 
-def clockwise():
+def lower():
+  RPL._____(#, #)
+  RPL._____(#, #)
+
+def rise():
   RPL.
   RPL.
 
-def counterclockwise():
-  RPL.
-  RPL.
+key = ''
 
-clockwise()
-
-counterclockwise()
+while key != ord('q'):
+  key = screen.getch()
+  screen.clear()
+  screen.addstr('q - quit | a - lower | s - rise')
+  if key == ord('q'):
+    curses.endwin()
+  if key == ord('a'):
+    lower()
+  if key == ord('s'):
+    rise()
