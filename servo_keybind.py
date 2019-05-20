3a3b3002e9c863140c3382d1b1457b89edfc4bfd
jmunkeby19 <34284770+jmunkeby19@users.noreply.github.com>
2019-05-20 14:31:22 -0500

    Update servo_keybind.py

--- servo_keybind.py
+++ servo_keybind.py
@@ -7,16 +7,51 @@ screen = curses.initscr()
 curses.halfdelay(5)
 curses.noecho()
 
-t1 = 0.5
-t2 = 1
-t3 = 2
-t4 = 4
+t1 = 4        #360
+t2 = 2        #180
+t3 = 1        #90
+t4 = 0.5      #45
+t5 = 0.166    #15
+t6 = 0.111    #10
+t7 = 0.055    #5
 
-def rotation():
+def clock_360():
+  RPL.servoWrite(1,2000)
+  time.sleep(t1)
+  RPL.servoWrite(1,0)
+
+def clock_180():
+  RPL.servoWrite(1,2000)
+  time.sleep(t2)
+  RPL.servoWrite(1,0)
+
+def clock_90():
+  RPL.servoWrite(1,2000)
+  time.sleep(t3)
+  RPL.servoWrite(1,0)
+
+def clock_45():
   RPL.servoWrite(1,2000)
   time.sleep(t4)
   RPL.servoWrite(1,0)
-  
+
+def clock_15():
+  RPL.servoWrite(1,2000)
+  time.sleep(t5)
+  RPL.servoWrite(1,0)
+
+def clock_10():
+  RPL.servoWrite(1,2000)
+  time.sleep(t6)
+  RPL.servoWrite(1,0)
+
+def clock_5():
+  RPL.servoWrite(1,2000)
+  time.sleep(t7)
+  RPL.servoWrite(1,0)
+
+
+
 key = ''
 
 while key != ord('q'):
@@ -26,4 +61,31 @@ while key != ord('q'):
   if key == ord('q'):
     curses.endwin()
   if key == ord('a'):
-    rotation()
+    clock_360()
+  if key == ord('s'):
+    clock_180()
+  if key == ord('d'):
+    clock_90()
+  if key == ord('f'):
+    clock_45()
+  if key == ord('g'):
+    clock_15()
+  if key == ord('h'):
+    clock_10()
+  if key == ord('j'):
+    clock_5()
+
+  if key == ord('z'):
+    cclock_360()
+  if key == ord('x'):
+    cclock_180()
+  if key == ord('c'):
+    cclock_90()
+  if key == ord('v'):
+    cclock_45()
+  if key == ord('b'):
+    cclock_15()
+  if key == ord('n'):
+    cclock_10()
+  if key == ord('m'):
+    cclock_5()
