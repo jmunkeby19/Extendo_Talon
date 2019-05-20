c7cd595a79524fac19e8ffdde1562482d34651d8
jmunkeby19 <34284770+jmunkeby19@users.noreply.github.com>
2019-05-20 10:24:14 -0500

    Update servo_keybind.py

--- servo_keybind.py
+++ servo_keybind.py
@@ -3,4 +3,23 @@ import time
 import curses
 import setup
 
-RPL.servoWrite(1,2000)
+t1 = 0.5
+t2 = 1
+t3 = 2
+t4 = 4
+
+def 360():
+  RPL.servoWrite(1,2000)
+  time.sleep(t4)
+  RPL.servoWrite(1,0)
+  
+key = ''
+
+while key != ord('q'):
+  key = screen.getch()
+  screen.clear()
+  screen.addstr('q - quit | a - 360 degrees')
+  if key == ord('q'):
+    curses.endwin()
+  if key == ord('a'):
+    360()
